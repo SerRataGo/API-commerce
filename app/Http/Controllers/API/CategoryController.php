bd87b7bd86a08b4c000145a1a59ab940498fdf6e
hebatallah ibrahim <heba_walid1012@gmail.com>
2022-03-03 14:28:59 +0200

    add function CategoryUpdate

--- app/Http/Controllers/API/CategoryController.php
+++ app/Http/Controllers/API/CategoryController.php
@@ -75,7 +75,43 @@ else{
 
     public function CategoryUpdate(Request $request, $id)
     {
-        //
+        $validator = Validator::make($request->all(),[
+            'category_name_en'=>'required|max:191',
+            'category_name_ar'=>'required|max:191',
+            'category_descripition_en'=>'required|max:191',
+            'category_descripition_ar'=>'required|max:191',
+            'category_icon'=>'required',
+
+        ]);
+        if($validator->fails()){
+            return response()->json([
+                'status'=>400,
+                'errors'=>$validator->messages()
+            ]);
+            }
+else{
+        $category = Category::find($id);
+        if($id){
+            $category->category_name_en=$request->input('category_name_en');
+            $category->category_name_ar=$request->input('category_name_ar');
+            $category->category_descripition_en=$request->input('category_descripition_en');
+            $category->category_descripition_ar=$request->input('category_descripition_ar');
+            $category->category_icon=$request->input('category_icon');
+             $category->status=$request->input('status')== true ? '1':'0' ;
+            $category->save();
+        return response()->json([
+            'status'=>200,
+            'message'=>'category updated succesfully'
+        ]);
+    }
+    else {
+        return response()->json([
+            'status'=>404,
+            'message'=>'No category id found'
+        ]);
+
+    }
+    }
     }
 
     public function CategoryDelete($id)
