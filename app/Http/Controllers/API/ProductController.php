393572e073b11a0075c4c1d6609dd6ab0e1c5f4b
hebatallah ibrahim <heba_walid1012@gmail.com>
2022-03-03 16:34:20 +0200

    add function UpdateProduct

--- app/Http/Controllers/API/ProductController.php
+++ app/Http/Controllers/API/ProductController.php
@@ -140,7 +140,88 @@ class ProductController extends Controller
 
     public function UpdateProduct(Request $request, $id)
     {
-        //
+        $validator = Validator::make($request->all(),[
+            'brand_id'=>'required',
+            'category_id'=>'required',
+           'sub_category_id'=>'required',
+           'product_name_en'=>'required|max:191',
+           'product_name_ar'=>'required|max:191',
+           'product_code'=>'required|max:20',
+           'product_qty'=>'required|max:20',
+           'product_tags_en'=>'required|max:20',
+           'product_tags_ar'=>'required|max:20',
+           'product_size_en'=>'required|max:191',
+           'product_size_ar'=>'required|max:191',
+           'product_color_en'=>'required|max:191',
+           'product_color_ar'=>'required|max:191',
+           'selling_price'=>'required|max:20',
+           'discount_price'=>'required|max:20',
+           'short_description_en'=>'required|max:191',
+           'short_description_ar'=>'required|max:191',
+           'description_en'=>'required|max:191',
+           'description_ar'=>'required|max:191',
+           'product_thumbnail'=>'required',
+           'hot_deals'=>'required',
+           'featured'=>'required',
+           'special_offer'=>'required',
+           'special_deals'=>'required',
+           'digital_file'=>'required',
+            
+    
+            
+        ]);
+        if($validator->fails()){
+            return response()->json([
+                'status'=>422,
+                'errors'=>$validator->messages()
+            ]);
+            }
+            else {
+       $product = Product::find($id) ;
+       if($id){
+        $product = new Product ;
+        $product->brand_id=$request->input('brand_id');
+        $product->category_id=$request->input('category_id');
+        $product->sub_category_id=$request->input('sub_category_id');
+        $product->product_name_en=$request->input('product_name_en');
+        $product->product_name_ar=$request->input('product_name_ar');
+        $product->product_code=$request->input('product_code');
+        $product->product_qty=$request->input('product_qty');
+        $product->product_tags_en=$request->input('product_tags_en');
+        $product->product_tags_ar=$request->input('product_tags_ar');
+        $product->product_size_en=$request->input('product_size_en');
+        $product->product_size_ar=$request->input('product_size_ar');
+        $product->product_color_en=$request->input('product_color_en');
+        $product->product_color_ar=$request->input('product_color_ar');
+        $product->selling_price=$request->input('selling_price');
+        $product->discount_price=$request->input('discount_price');
+        $product->short_description_en=$request->input('short_description_en');
+        $product->short_description_ar=$request->input('short_description_ar');
+        $product->description_en=$request->input('description_en');
+        $product->description_ar=$request->input('description_ar');
+        $product->product_thumbnail=$request->input('product_thumbnail');
+        $product->hot_deals=$request->input('hot_deals');
+        $product->featured=$request->input('hot_deals');
+        $product->special_offer=$request->input('special_offer');
+        $product->special_deals=$request->input('special_deals');
+        $product->digital_file=$request->input('digital_file');
+        $product->status=$request->input('status') == true ? '1':'0';;
+       $product->update();
+       return response()->json([
+        'status'=>200,
+        'message'=>'product updated succesfully'
+    ]);
+       }
+       else {
+        return response()->json([
+            'status'=>404,
+            'message'=>'No product found'
+        ]);
+       }
+            }
+    
+    
+    
     }
 
     public function DeleteProduct($id)
