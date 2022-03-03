a84b1ea92b49036c33ee0f0f281b19e976aec851
hebatallah ibrahim <heba_walid1012@gmail.com>
2022-03-03 15:07:13 +0200

    add the relation in Category model

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\Product;
 
 class Category extends Model
 {
@@ -18,4 +19,7 @@ class Category extends Model
         'status'
         
     ];
+    public function product(){
+        return $this->hasMany(Product::class);
+    }
 }
