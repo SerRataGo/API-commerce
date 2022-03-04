37a8a782b076eb809621571165271ee14910b321
hebatallah ibrahim <heba_walid1012@gmail.com>
2022-03-04 18:34:54 +0200

    add function StoreCoupon

--- app/Http/Controllers/API/CouponController.php
+++ app/Http/Controllers/API/CouponController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\API;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Coupon;
+use Illuminate\Support\Facades\Validator;
 
 class CouponController extends Controller
 {
@@ -20,6 +21,31 @@ class CouponController extends Controller
 
     public function StoreCoupon(Request $request)
     {
+        $validator = Validator::make($request->all(),[
+            'coupon_name'=>'required|max:191',
+            'coupon_discount'=>'required',
+            'coupon_validity'=>'required',
+           
+
+        ]);
+        if($validator->fails()){
+            return response()->json([
+                'status'=>400,
+                'errors'=>$validator->messages()
+            ]);
+            }
+else{
+        $coupon = new Coupon;
+        $coupon->coupon_name=$request->input('coupon_name');
+        $coupon->coupon_discount=$request->input('coupon_discount');
+        $coupon->coupon_validity=$request->input('coupon_validity');
+        $coupon->status=$request->input('status')== true ? '1':'0' ;
+        $coupon->save();
+        return response()->json([
+            'status'=>200,
+            'message'=>'Coupon added succesfully'
+        ]);
+    }
 
     }
 
