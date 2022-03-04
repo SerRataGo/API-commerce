75721f0daecee24f7e3693d92a4979d644e56667
hebatallah ibrahim <heba_walid1012@gmail.com>
2022-03-04 21:31:20 +0200

    add function StoreAdminUser

--- app/Http/Controllers/API/AdminUserController.php
+++ app/Http/Controllers/API/AdminUserController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers\API;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\User;
+use Illuminate\Support\Facades\Validator;
+
 
 class AdminUserController extends Controller
 {
@@ -26,7 +28,34 @@ class AdminUserController extends Controller
 
     public function StoreAdminUser(Request $request)
     {
-        //
+        $validator = Validator::make($request->all(),[
+            'name'=>'required',
+             'email'=>'required',
+            'password'=>'required',
+            'last_seen'=>'required',
+
+        ]);
+        if($validator->fails()){
+            return response()->json([
+                'status'=>422,
+                'errors'=>$validator->messages()
+            ]);
+            }
+            else {
+       $user = new User ;
+       $user->name=$request->input('name');
+       $user->email=$request->input('email');
+       $user->phone=$request->input('phone');
+       $user->last_seen=$request->input('last_seen');
+       $user->email_verified_at=$request->input('email_verified_at');
+       $user->password=$request->input('password');
+       $user->current_team_id=$request->input('current_team_id');
+       $user->save();
+       return response()->json([
+        'status'=>200,
+        'message'=>'user added succesfully'
+    ]);
+            }
     }
 
     public function EditAdminUser($id)
