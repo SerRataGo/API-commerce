a073d2ac4f04ca0b7fd770ca0763bf915ab67e6a
hebatallah ibrahim <heba_walid1012@gmail.com>
2022-03-09 13:43:40 +0200

    Delete Slider

--- app/Http/Controllers/API/SliderController.php
+++ app/Http/Controllers/API/SliderController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers\API;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Intervention\Image\Facades\Image;
+use App\Models\Slider;
 
 class SliderController extends Controller
 {
@@ -29,6 +31,22 @@ class SliderController extends Controller
 
     public function DeleteSlider($id)
     {
+        $slider = Slider::find($id);
+        if($slider){
+        unlink($slider->slider_image);
+        $slider->delete();
+
+        return response()->json([
+            'status'=>200,
+            'message'=>'slider deleted succesfully'
+    
+        ]);}
+        else{
+            return response()->json([
+                'status'=>404,
+                'message'=>'Slider ID not found'
+            ]);
+        }
 
     }
 
