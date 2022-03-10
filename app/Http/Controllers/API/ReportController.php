7560599176414ded87c695c8028a7e9d5defe1f4
hebatallah ibrahim <heba_walid1012@gmail.com>
2022-03-11 01:14:15 +0200

    ReportSearchByDate

--- app/Http/Controllers/API/ReportController.php
+++ app/Http/Controllers/API/ReportController.php
@@ -3,18 +3,29 @@
 namespace App\Http\Controllers\API;
 
 use App\Http\Controllers\Controller;
+use DateTime;
+use App\Models\Order;
+use Illuminate\Http\Request;
 
 class ReportController extends Controller
 {
 
     public function ViewReport()
     {
-        //
+       //view
     }
 
-    public function ReportSearchByDate()
+    public function ReportSearchByDate(Request $request)
     {
-        //
+        $date = new DateTime($request->date);
+        $formatDate = $date->format('d F Y');
+        
+
+        $orders = Order::where('order_date', $formatDate)->latest()->get();
+        return response()->json([
+            'status' => 200,
+            'orders' => $orders,
+        ]);
     }
 
     public function ReportSearchByMonth()
